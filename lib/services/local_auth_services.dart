503170098ab9d7647cd559be3e5851427b8816a3
Khubaib Jamal <khubaib.j18@gmail.com>
2023-11-19 22:36:07 +0500

    refactor fingerprint service

--- lib/services/local_auth_services.dart
+++ lib/services/local_auth_services.dart
@@ -2,22 +2,27 @@ import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 
 class LocalAuthService {
+  // local auth instance
   static final LocalAuthentication auth = LocalAuthentication();
 
+  // method to check device supported biometric
   static Future<bool> canAuthenticate() async {
     return await auth.canCheckBiometrics || await auth.isDeviceSupported();
   }
 
+  // method to authenticate
   static Future<bool> authenticate() async {
     try {
-      if (!await canAuthenticate()) {
-        print("object");
-        return false;
-      }
+      // device is not supported biometric return false
+      if (!await canAuthenticate()) return false;
+
+      // method to authenticate
       return await auth.authenticate(
         localizedReason: "Scan fingerprint to authenticate",
         options: const AuthenticationOptions(
+          // if fingerprint is not register
           useErrorDialogs: true,
+          // open from background again ask for fingerprint
           stickyAuth: true,
         ),
       );
